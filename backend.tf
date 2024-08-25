935b7d35206aedcbb31ed31182e3aa16a2784614
dipanshurai.11@gmail.com <dipanshurai.11@gmail.com>
2024-08-25 11:17:11 +0530

    clean

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "tg-devops-11am-weekend-july-terraform-state"
-    key = "terraform.tfstate"
-    region = "us-west-2"
-    
-  }
-}
--- provider.tf
+++
@@ -1,8 +0,0 @@
-provider "aws" {
-    region = "us-west-2"  
-
-    assume_role {
-      role_arn = "arn:aws:iam::211125425717:role/devops-11am-weekend-july-terraform-role"
-      session_name = "devops-11"
-    }
-}
